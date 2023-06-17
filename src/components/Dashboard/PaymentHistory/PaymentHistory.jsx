71da7a19bd7487acfa567df24e546dc38f41ebd4
AhmedRahi <rezarahi71@gmail.com>
2023-06-17 17:47:42 +0600

    api added

--- src/components/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/components/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -1,7 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 
 const PaymentHistory = () => {
-   
+    const { data: history = [] } = useQuery(["history"], async () => {
+        const res = await fetch("http://localhost:5000/payments");
+        return res.json();
+      });
   return (
     <div>
       <h2 className="text-black text-5xl font-serif border-b-2 border-b-black pb-2 text-center mb-5 mt-5 mx-52">
