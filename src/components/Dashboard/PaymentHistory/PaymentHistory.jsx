442ac8213d7d501fd4b4b3028654c7b7b3fc6478
AhmedRahi <rezarahi71@gmail.com>
2023-06-17 17:47:10 +0600

    payment file added

--- src/components/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/components/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -4,7 +4,9 @@ const PaymentHistory = () => {
    
   return (
     <div>
-      
+      <h2 className="text-black text-5xl font-serif border-b-2 border-b-black pb-2 text-center mb-5 mt-5 mx-52">
+        Payment History
+      </h2>
       
     </div>
   );
